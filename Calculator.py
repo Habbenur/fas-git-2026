578533dbf1134e0bc353c88433302ee47287c8c0
Habbenur <habbenurozen@gmail.com>
2026-01-14 10:19:40 +0100

    Här commer en app för att kalkyla

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+#här kommer en kalkylator
+def add(a, b):
+    return a + b    
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+#här kommer kommentarer på svenska
